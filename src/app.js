11d41d0363c406f1e97eb5ecc28fb4bffb21dfb0
AnaAIHS <anahersor@gmail.com>
2021-07-01 22:05:47 -0500

    Otra prueba de deployment

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 app.set('view engine', 'ejs');
 
-const publicPath= path.resolve(__dirname, './public')
+const publicPath= path.resolve(__dirname, '../public')
 app.use(express.static(publicPath)); //tener carpeta public como recurso estático para poder usar sus elementos de manera sencilla
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('../../routes/index');
 app.use('/', indexRouter);
 
 app.listen(process.env.PORT || 3000, function(){
